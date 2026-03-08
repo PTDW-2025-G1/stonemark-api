cc9005b0b23286259de99dab5e5c55291e32f2c6
Ratmir Mukazhanov <ratmir.mukazhanov@devlop.systems>
2026-03-08 18:07:57 +0000

    refactor(account): remove contact management methods from AccountService and AccountServiceImpl

--- user/src/main/java/pt/estga/user/mappers/UserMapper.java
+++ user/src/main/java/pt/estga/user/mappers/UserMapper.java
@@ -1,6 +1,5 @@
 package pt.estga.user.mappers;
 
-import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import pt.estga.user.dtos.ProfileUpdateRequestDto;
@@ -8,7 +7,6 @@ import pt.estga.user.dtos.UserPublicDto;
 import pt.estga.user.dtos.UserDto;
 import pt.estga.user.entities.User;
 
-@Mapper(componentModel = "spring", uses = UserContactMapper.class)
 public interface UserMapper {
 
     @Mapping(target = "photoId", source = "photo.id")
--- user/src/main/java/pt/estga/user/services/AccountService.java
+++ user/src/main/java/pt/estga/user/services/AccountService.java
@@ -1,26 +1,10 @@
 package pt.estga.user.services;
 
 import pt.estga.user.dtos.AccountSecurityStatusDto;
-import pt.estga.user.dtos.LinkedProviderDto;
 import pt.estga.user.entities.User;
-import pt.estga.user.entities.UserContact;
-import pt.estga.user.enums.ContactType;
-import pt.estga.user.enums.Provider;
-
-import java.util.List;
 
 public interface AccountService {
 
-    void addContact(User user, String value, ContactType type);
-
-    void requestContactVerification(User user, Long contactId);
-
-    List<UserContact> getContacts(User user);
-
     AccountSecurityStatusDto getSecurityStatus(User user);
-
-    void setPrimaryContact(User user, Long contactId);
-
-    void deleteContact(User user, Long contactId);
 
 }
