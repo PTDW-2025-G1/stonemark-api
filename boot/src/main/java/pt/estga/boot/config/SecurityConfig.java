45399d21a9dcca2e824cc583fb7794f5326237c3
Ratmir Mukazhanov <ratmir.mukazhanov@devlop.systems>
2026-03-08 21:17:37 +0000

    refactor(security): remove legacy authentication logic and enable Keycloak-only mode

--- boot/src/main/java/pt/estga/boot/config/SecurityConfig.java
+++ boot/src/main/java/pt/estga/boot/config/SecurityConfig.java
@@ -5,12 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import pt.estga.auth.services.LogoutService;
 
@@ -50,9 +48,8 @@ public class SecurityConfig {
             "https://*.stonemark.pt"
     };
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final AuthenticationProvider authenticationProvider;
     private final LogoutService logoutService;
+    private final KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -82,17 +79,17 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(keycloakJwtAuthenticationConverter)
+                        )
+                )
                 .logout(logout -> logout
                         .logoutUrl("/api/v1/auth/logout")
                         .addLogoutHandler(logoutService)
                         .logoutSuccessHandler(
-                                (
-                                        request,
-                                        response,
-                                        authentication
-                                ) -> response.setStatus(HttpServletResponse.SC_OK)
+                                (request, response, authentication) ->
+                                        response.setStatus(HttpServletResponse.SC_OK)
                         )
                 );
 
