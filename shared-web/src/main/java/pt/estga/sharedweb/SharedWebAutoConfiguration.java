54350c949f444266670093c8732ce86873e5589a
Ratmir Mukazhanov <ratmir.mukazhanov@devlop.systems>
2026-03-21 23:16:55 +0000

    refactor: introduce shared-web module with auto-configuration and update parent pom

---
+++ shared-web/src/main/java/pt/estga/sharedweb/SharedWebAutoConfiguration.java
@@ -0,0 +1,9 @@
+package pt.estga.sharedweb;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pt.estga.sharedweb")
+public class SharedWebAutoConfiguration {
+}
