396cbf21a0f78fca8d23576cdd3783181190063f
Ratmir Mukazhanov <ratmir.mukazhanov@devlop.systems>
2026-03-21 06:49:52 +0000

    refactor: introduce filter-utils module with Spring auto-configuration support

---
+++ filter-utils/src/main/java/pt/estga/filterutils/FilterUtilsModuleAutoConfiguration.java
@@ -0,0 +1,9 @@
+package pt.estga.filterutils;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pt.estga.filterutils")
+public class FilterUtilsModuleAutoConfiguration {
+}
