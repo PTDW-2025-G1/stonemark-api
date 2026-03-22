b483f0e213e51ef8171f89d036bc0d677ea56146
Ratmir Mukazhanov <ratmir.mukazhanov@devlop.systems>
2026-03-22 16:49:26 +0000

    refactor: add notification module with auto-configuration and update parent pom and dependencies

---
+++ notification/src/main/java/pt/estga/notification/NotificationAutoConfiguration.java
@@ -0,0 +1,9 @@
+package pt.estga.notification;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@AutoConfiguration
+@ComponentScan("pt.estga.notification")
+public class NotificationAutoConfiguration {
+}
