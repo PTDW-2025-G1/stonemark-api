65ee42f185c6cd6a58c78922e817ddb2c5d4cbc0
Ratmir Mukazhanov <ratmir.mukazhanov@devlop.systems>
2026-05-30 00:45:06 +0100

    feat(authorization): add configuration for OAuth2 authorization server

--- boot/src/main/java/pt/estga/boot/config/SecurityConfig.java
+++ boot/src/main/java/pt/estga/boot/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -49,6 +50,7 @@ public class SecurityConfig {
     private final KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter;
 
     @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
