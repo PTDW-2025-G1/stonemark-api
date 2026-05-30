ae0fd3504c91746de9c740d24b2f7b8f46af9794
Ratmir Mukazhanov <ratmir.mukazhanov@devlop.systems>
2026-05-31 00:41:05 +0100

    refactor(user): remove filterable annotations from UserPublicDto fields

--- user/src/main/java/pt/estga/user/dtos/UserPublicDto.java
+++ user/src/main/java/pt/estga/user/dtos/UserPublicDto.java
@@ -1,13 +1,12 @@
 package pt.estga.user.dtos;
 
 import lombok.Builder;
-import pt.estga.sharedweb.annotations.Filterable;
 
 @Builder
 public record UserPublicDto(
         Long id,
-        @Filterable String username,
-        @Filterable String firstName,
-        @Filterable String lastName,
+        String username,
+        String firstName,
+        String lastName,
         Long photoId
 ) { }
