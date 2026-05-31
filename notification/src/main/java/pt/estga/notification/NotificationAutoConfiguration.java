5293e9a444e5fc90572af07a7f334d557b0d7f16
Ratmir Mukazhanov <ratmir.mukazhanov@devlop.systems>
2026-05-31 02:25:37 +0100

    refactor: centralize spring configuration and remove module auto-configs
    - Delete all module-specific `AutoConfiguration` classes and corresponding `AutoConfiguration.imports` files across all modules
    - Add `@EnableJpaRepositories` and `@EntityScan` to `StonemarkApplication` to globally scan the root `pt.estga` package
    - Relocate `MessageSource` and `RestTemplate` bean definitions to the centralized `ApplicationConfig` in the `boot` module

--- notification/src/main/java/pt/estga/notification/NotificationAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package pt.estga.notification;
-
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-@AutoConfiguration
-@ComponentScan("pt.estga.notification")
-public class NotificationAutoConfiguration {
-}
--- shared-web/src/main/java/pt/estga/sharedweb/config/SharedWebAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package pt.estga.sharedweb.config;
-
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-@AutoConfiguration
-@ComponentScan("pt.estga.sharedweb")
-public class SharedWebAutoConfiguration {
-}
